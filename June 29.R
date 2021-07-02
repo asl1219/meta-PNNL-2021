361bffac4d308265a8a18fa3f56c8474fc9248cb
AdamL <1219asl@gmail.com>
2021-07-02 02:24:19 -0400

    Organization, Reshaping, and Cleanup
    Cleaned up both datasets by removing unnecessary columns and removing rows with N/A values. Reshaped data to be tidy, with compacted dates and treatment info. Also set up convention of modifying dataset names to reflect state of the data.

--- June 29.R
+++ June 29.R
@@ -1,3 +1,17 @@
-#read in files
-readr::read_csv("fluxes.csv")
-readr::read_csv("collar_map.csv")
+#requiredlibraries
+library(tidyr)
+library(dplyr)
+
+#read in files and rename
+fluxes = readr::read_csv("fluxes.csv")
+collar_map = readr::read_csv("collar_map.csv")
+
+#reshape/cleanup data
+fluxesTidy = 
+  (fluxes %>% 
+    gather(date, value, 2:7) %>% 
+      filter(!is.na(value)))
+collarMapTidy = 
+  collar_map %>% select(-LI8100A_Port) %>%
+    unite("Treatment", 2:4, sep = ", ")
+      
