a0bb3946f848170dd614032fae3f97cc2a702ac4
AdamL <1219asl@gmail.com>
2021-07-03 02:52:38 -0400

    Cleaning up of Data, N/A, and Treatment Summary
    The N/A filter was removed from the reshaping block. The "Group" column was also removed due to it being irrelevant to the summary. Then, once the two sets were joined together, the N/A values were filted out. Finally, summarized the mean, st. dev., and number of values for each consolidated treatment.

--- June 29.R
+++ June 29.R
@@ -9,9 +9,18 @@ collar_map = readr::read_csv("collar_map.csv")
 #reshape/cleanup data
 fluxesTidy = 
   (fluxes %>% 
-    gather(date, value, 2:7) %>% 
-      filter(!is.na(value)))
+    gather(date, value, 2:7))
+#LI8100A is empty so remove it
 collarMapTidy = 
-  collar_map %>% select(-LI8100A_Port) %>%
-    unite("Treatment", 2:4, sep = ", ")
+  collar_map %>% select(-LI8100A_Port, -Group) %>%
+    unite("Treatment", 2:3, sep = ", ")
       
+#joining metadata and column cleanup and remove "N/A"
+collarInfo = 
+  collarMapTidy %>% left_join(fluxesTidy, "Collar") %>%
+    filter(!is.na(value))
+
+
+#print dataset summary by treatment
+collarInfo %>% group_by(Treatment) %>% summarize(mean(value), sd(value), n()) 
+
