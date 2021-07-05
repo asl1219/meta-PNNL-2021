e6698ab5c8a86cd97d854c4d71f5239cb3a59e10
AdamL <1219asl@gmail.com>
2021-07-04 21:44:50 -0400

    Accidentally unidid last commit, various explained in desc.
    Changed collar_map to only include treatment and not plot. Made sure that collarInfo was actually printed, Also changed the process of creating collarsInfo to create collarsNotIncluded at the same time. collarsInfo united the 2 original datasetts. A line graph using the means by both date and treatment was constructed with color by treatments and the series of measurements by date. Finally, printed out collarsNotIncluded.

--- June 29.R
+++ June 29.R
@@ -13,19 +13,31 @@ fluxesTidy =
     gather(date, value, 2:7))
 #LI8100A is empty so remove it
 collarMapTidy = 
-  collar_map %>% select(-LI8100A_Port, -Group) %>%
-    unite("Treatment", 2:3, sep = ", ")
+  collar_map %>% select(-LI8100A_Port, -Group, -Plot)
       
 #joining metadata and column cleanup and remove "N/A"
-collarInfo = 
-  collarMapTidy %>% left_join(fluxesTidy, "Collar") %>%
-    filter(!is.na(value))
+#also pull out collars that get filtered out
+  collarInfoPreFilter = 
+  collarMapTidy %>% left_join(fluxesTidy, "Collar")
 
+collarsNotIncluded = collarInfoPreFilter %>% filter(is.na(value)) %>% select(-value)
+
+collarInfo = collarInfoPreFilter  %>%
+  filter(!is.na(value))
 
 #print dataset summary by treatment
 collarInfo %>% group_by(Treatment) %>% summarize(mean(value), sd(value), n()) 
+print(collarInfo)
 
 #boxplot by treatment with dots
 collarInfo %>% ggplot(aes(value, Treatment)) + geom_boxplot() + geom_jitter(height = 0)#15 boxplots seems like a lot
 
+#line graph of mean fluxes by treatment and date by error bars
+#mean and s.d. by date/treatment (similar to printing info by treatment)
+meansByBoth = collarInfo %>% group_by(date, Treatment) %>% summarize(means = mean(value))
+StDevByBoth = collarInfo %>% group_by(date, Treatment) %>% summarize(stDevds = sd(value))
 
+meansByBoth %>% ggplot(aes(date, means, group = Treatment, color = Treatment)) + geom_line()
+
+#print the information on collars not included (later to be included in report)
+print(collarsNotIncluded)
