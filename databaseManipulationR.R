88010dd5762f4b5638f0ad2aa3bba5eb2fe1193d
AdamL <1219asl@gmail.com>
2021-07-11 03:08:36 -0400

    Found and reported number of entries by Ecosystem type
    First got rid of whitespace so all of the same biome would be included in the same biome group, then grouped by ecosystem and found the value of the number of entries. Reported info.

--- databaseManipulationR.R
+++ databaseManipulationR.R
@@ -7,3 +7,12 @@ srdbData = read.csv("srdb-master/srdb-data.csv")
 
 #choose relevant categories
 srdbManagable = srdbData%>% select(Latitude, Longitude, Study_midyear, Ecosystem_type, Manipulation, Rs_annual, RC_annual)
+
+#number of observations (rows) by ecosystem type
+#get rid of whitespace first
+srdbManagable$Ecosystem_type = srdbManagable$Ecosystem_type %>% trimws(c("both"))
+numberByEcoType = srdbManagable %>% group_by(Ecosystem_type) %>% summarize(n())
+
+
+#print/report
+numberByEcoType
