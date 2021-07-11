4a946275be431757f45499f0dfb6e0c0b7c69932
AdamL <1219asl@gmail.com>
2021-07-11 02:31:08 -0400

    Organizing and selecting categories
    Used comments to organize code and selected out the columns/categories relevant to future manipulation.

--- databaseManipulationR.R
+++ databaseManipulationR.R
@@ -1,5 +1,9 @@
+#necessary libraries
 library(tidyr)
 library(dplyr)
 
+#read in data
 srdbData = read.csv("srdb-master/srdb-data.csv")
-  
+
+#choose relevant categories
+srdbManagable = srdbData%>% select(Latitude, Longitude, Study_midyear, Ecosystem_type, Manipulation, Rs_annual, RC_annual)
